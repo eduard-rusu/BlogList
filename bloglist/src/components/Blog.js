21acd13178c416154ef79f598bf7d07a36ed8b2d
RUSU EDUARD <rusu_eduard@live.com>
2023-03-09 15:07:35 +0200

    Added detailed display to each blog

--- bloglist/src/components/Blog.js
+++ bloglist/src/components/Blog.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+
 const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const details = {display: showDetails ? '' : 'none'}
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
   return (
-    <div>
-      { blog.title } { blog.author }
+    <div style={blogStyle}>
+      { blog.title } 
+      { blog.author }
+      <button onClick={toggleDetails}>text</button>
+      <div style={details}>
+        <div>
+          { blog.url }
+        </div>
+        <div>
+          { blog.likes }
+          <button>like</button>
+        </div>
+        <div>
+          { blog.user.username}
+        </div>
+      </div>
     </div>
   )
 }
